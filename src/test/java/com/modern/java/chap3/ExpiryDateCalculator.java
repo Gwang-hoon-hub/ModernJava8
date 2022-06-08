b200e88512547add4c9ea03a25a569afc57838bd
panghoon <leegwanghoon95@gmail.com>
2022-06-08 20:06:38 +0900

    [FEAT] : TDD chapter3 and confirm commit

--- src/test/java/com/modern/java/chap3/ExpiryDateCalculator.java
+++ src/test/java/com/modern/java/chap3/ExpiryDateCalculator.java
@@ -8,5 +8,4 @@ public class ExpiryDateCalculator {
         return billingDate.plusMonths(1);
     }
 
-
 }
--- src/test/java/com/modern/java/chap3/ExpiryDateCalculatorTest.java
+++ src/test/java/com/modern/java/chap3/ExpiryDateCalculatorTest.java
@@ -19,21 +19,6 @@ public class ExpiryDateCalculatorTest {
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
-//        LocalDate billingDate = LocalDate.of(2019, 3, 1);
-//        int payAmount = 10_000;
-//        ExpiryDateCalculator cal = new ExpiryDateCalculator();
-//        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-//
-//        assertEquals(LocalDate.of(2019,4,1), expiryDate);
-//
-//        // 예를 추가하면서 구현 일반화
-//        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-//        int payAmount2 = 10_000;
-//
-//        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-//        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-//
-//        assertEquals(LocalDate.of(2019,6,5), expiryDate2);
 
         assertExpiryDate(
                 LocalDate.of(2019,3,1), 10_000,
@@ -63,7 +48,5 @@ public class ExpiryDateCalculatorTest {
                 LocalDate.of(2019,5,31), 10_000,
                 LocalDate.of(2019, 6, 30)
         );
-
-
     }
 }
