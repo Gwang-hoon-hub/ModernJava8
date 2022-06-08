c7d6b21453e6ca33ebdea4eac6c8ad6d24ce12d0
leegwanghoon95@gmail.com <rhkdgns9@@>
2022-06-08 09:59:36 +0900

    [FEAT] : TDD PasswordStrength

---
+++ src/test/java/com/modern/java/chap2/PasswordStrength.java
@@ -0,0 +1,5 @@
+package com.modern.java.chap2;
+
+public enum PasswordStrength {
+    STRONG, NORMAL, WEAK;
+}
--- src/test/java/com/modern/java/chap2/PasswordStrengthMeterTest.java
+++ src/test/java/com/modern/java/chap2/PasswordStrengthMeterTest.java
@@ -1,11 +1,35 @@
 package com.modern.java.chap2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordStrengthMeterTest {
 
     @Test
-    void _name(){
-
+    void meetsAllCriteria_Then_Strong(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("asdASD1@#");
+        assertEquals(PasswordStrength.STRONG, result);
+        PasswordStrength result2 = meter.meter("asd1@#ASD");
+        assertEquals(PasswordStrength.STRONG, result2);
     }
+
+    @Test
+    void meetsOtherCriteria_excect_for_Length_Then_Normal(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("as!@12AS");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab!@#AS!@#");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+
+
+
 }
