95be0ea39d862c1af36eaa229cbd026c7cf2a86f
leegwanghoon95@gmail.com <rhkdgns9@@>
2022-06-09 10:44:08 +0900

    [FEAT] :TDD Chapter3 - Add candidateExp using

--- src/test/java/com/modern/java/chap3/ExpiryDateCalculatorTest.java
+++ src/test/java/com/modern/java/chap3/ExpiryDateCalculatorTest.java
@@ -17,6 +17,12 @@ public class ExpiryDateCalculatorTest {
 
     // 쉬운것 부터 테스트
 
+    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate){
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
+        assertEquals(expectedExpiryDate, realExpiryDate);
+    }
+
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
 
@@ -27,12 +33,6 @@ public class ExpiryDateCalculatorTest {
                         .build(),
                 LocalDate.of(2019,4,1)
         );
-    }
-
-    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate){
-        ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
-        assertEquals(expectedExpiryDate, realExpiryDate);
     }
 
     @Test
@@ -52,4 +52,27 @@ public class ExpiryDateCalculatorTest {
                 LocalDate.of(2019,2,28)
         );
     }
+
+    @Test
+    void 첫_납부일과_만료일_일자가_다를때_만원_납부(){
+        PayData payData = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019,1,31))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(10_000)
+                .build();
+        assertExpiryDate(payData, LocalDate.of(2019,3,31));
+
+        PayData payData2 = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019,1,30))
+                .billingDate(LocalDate.of(2019,2,28))
+                .payAmount(10_000)
+                .build();
+        assertExpiryDate(payData2, LocalDate.of(2019, 3, 30));
+    }
+
+
+
+
+
+
 }
