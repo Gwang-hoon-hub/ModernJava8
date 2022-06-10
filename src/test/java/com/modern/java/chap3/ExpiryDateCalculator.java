721c5b1a6a985f031de94ab350236f11f02c0112
leegwanghoon95@gmail.com <rhkdgns9@@>
2022-06-10 10:12:24 +0900

    [FEAT] :TDD Chapter3 refactoring

--- src/test/java/com/modern/java/chap3/ExpiryDateCalculator.java
+++ src/test/java/com/modern/java/chap3/ExpiryDateCalculator.java
@@ -1,31 +1,39 @@
 package com.modern.java.chap3;
 
-import org.springframework.cglib.core.Local;
-
 import java.time.LocalDate;
-import java.time.YearMonth;
-
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addedMonths = payData.getPayAmount()/10_000;
+        int addedMonths = payData.getPayAmount() / 10_000;
         if(payData.getFirstBillingDate() != null){
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if(payData.getFirstBillingDate().getDayOfMonth() !=
-            candidateExp.getDayOfMonth()){
-                final int dayLenOfCandiMon =
-                        YearMonth.from(candidateExp).lengthOfMonth();
-            // 첫 납부일과 이후 후보 납부 일자가 맞지 않는 경우
-                if(dayLenOfCandiMon <
-                payData.getFirstBillingDate().getDayOfMonth()){
-            // 2만원 이상의 금액을 납부했을 경우 첫 납부일과 만료일자가 다른 경우
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                }
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth());
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else{
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths){
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        if(isSameDayMonth(payData.getFirstBillingDate(), candidateExp)){
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            // 첫 납부일과 이후 후보 납부 일자가 맞지 않는 경우
+            if(dayLenOfCandiMon < dayOfFirstBilling){
+                // 2만원 이상의 금액을 납부했을 경우 첫 납부일과 만료일자가 다른 경우
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isSameDayMonth(LocalDate date1, LocalDate date2){
+        if(date1.getDayOfMonth() != date2.getDayOfMonth()) return false;
+        else return true;
+    }
+
+    private int lastDayOfMonth(LocalDate date){
+        return date.lengthOfMonth();
     }
 
 }
