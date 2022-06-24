5076bf7cd22cbc4fb115caeda0c2da2504fda38b
panghoon <leegwanghoon95@gmail.com>
2022-06-24 15:58:32 +0900

    before refactoring

--- src/test/java/com/modern/java/chap4/NoteHighlightTest.java
+++ src/test/java/com/modern/java/chap4/NoteHighlightTest.java
@@ -16,6 +16,8 @@ public class NoteHighlightTest {
         assertThat(highlight("note")).isEqualTo("{note}");
         assertThat(highlight("1 note")).isEqualTo("1 {note}");
         assertThat(highlight("1 note 2")).isEqualTo("1 {note} 2");
+        assertThat(highlight("keynote")).isEqualTo("keynote");
+        assertThat(highlight("ke1note")).isEqualTo("ke1note");
     }
 
     private String highlight(String str) {
@@ -23,7 +25,16 @@ public class NoteHighlightTest {
         if(idx == -1 ){
             return str;
         }
+        int preChIdx = idx - 1;
+        if(preChIdx >= 0) {
+            char pre = str.charAt(preChIdx);
+            if (IsNotSpace(pre)) return str;
+        }
         return str.replace("note", "{note}");
+    }
+
+    private boolean IsNotSpace(char pre) {
+        return (pre >= 'a' && pre <= 'y') || (pre >= '0' && pre <= '9');
     }
 
 }
