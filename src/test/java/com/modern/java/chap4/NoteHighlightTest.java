1669437a2d66a31fdb931e39b8b053ce6eb9b313
panghoon <leegwanghoon95@gmail.com>
2022-06-24 15:46:43 +0900

    [FEAT] : refactoring use indexof, replace

--- src/test/java/com/modern/java/chap4/NoteHighlightTest.java
+++ src/test/java/com/modern/java/chap4/NoteHighlightTest.java
@@ -15,13 +15,15 @@ public class NoteHighlightTest {
         assertThat(highlight("efg")).isEqualTo("efg");
         assertThat(highlight("note")).isEqualTo("{note}");
         assertThat(highlight("1 note")).isEqualTo("1 {note}");
+        assertThat(highlight("1 note 2")).isEqualTo("1 {note} 2");
     }
 
     private String highlight(String str) {
-        if (str.equals("note")) return "{note}";
-        else if (str.equals("1 note")) return "1 {note}";
-        return str;
+        int idx = str.indexOf("note");
+        if(idx == -1 ){
+            return str;
+        }
+        return str.replace("note", "{note}");
     }
-
 
 }
