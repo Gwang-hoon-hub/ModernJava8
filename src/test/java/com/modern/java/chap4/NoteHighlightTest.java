f3071a4d93c231f9391751da1518863be5ac7ea1
panghoon <leegwanghoon95@gmail.com>
2022-06-25 20:08:03 +0900

    before ispreChNotSpace refactoring

--- src/test/java/com/modern/java/chap4/NoteHighlightTest.java
+++ src/test/java/com/modern/java/chap4/NoteHighlightTest.java
@@ -18,28 +18,37 @@ public class NoteHighlightTest {
         assertThat(highlight("1 note 2")).isEqualTo("1 {note} 2");
         assertThat(highlight("keynote")).isEqualTo("keynote");
         assertThat(highlight("ke1note")).isEqualTo("ke1note");
+        assertThat(highlight("yes note1")).isEqualTo("yes note1");
     }
 
     private String highlight(String str) {
-        int idx = str.indexOf("note");
+        int idx = str.indexOf("note"); // 찾고자 하는 값이 indexOf 에 있으면 0을 return 한다. 없으면 -1 return
         if(idx == -1 ){
             return str;
         }
         // if(isPreChNotSpace(str, idx) return str          --> 리팩토링을 위한 코드
-        boolean preChNotSpace = isPreChNotSpace(str, idx);
-        if(preChNotSpace) return str;
+        if(isPreChNotSpace(str, idx)) return str;
+        if(isPostChNotSpace(str, idx)) return str;
         return str.replace("note", "{note}");
+    }
+
+    private boolean isPostChNotSpace(String str, int idx) {
+        boolean postChNotSpace = false;
+        int postChIdx = idx + "note".length();
+        if(postChIdx < str.length()) {  // idx 의 길이가 str의 길이보다 작아야 한다.
+            char post = str.charAt(postChIdx);
+            if (IsNotSpace(post)) {
+                postChNotSpace = true;
+            }
+        }
+        return postChNotSpace;
     }
 
     private boolean isPreChNotSpace(String str, int idx) {
         boolean preChNotSpace = false;
         int preChIdx = idx - 1;
-        if(preChIdx >= 0) {
-            char pre = str.charAt(preChIdx);
-            if (IsNotSpace(pre)) {
+        if(preChIdx >= 0 && IsNotSpace(str.charAt(preChIdx))) {
                 preChNotSpace = true;
-
-            }
         }
         return preChNotSpace;
     }
@@ -47,5 +56,4 @@ public class NoteHighlightTest {
     private boolean IsNotSpace(char pre) {
         return (pre >= 'a' && pre <= 'y') || (pre >= '0' && pre <= '9');
     }
-
 }
